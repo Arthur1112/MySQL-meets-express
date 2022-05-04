e7c942de38b5cbfb36b48b9e80faaaa56a045990
Arthur <arthur_2335@hotmail.com>
2022-05-04 15:05:24 -0400

    imported createdPet

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { getPets } from "./src/pets.js";
+import { getPets, createPet } from "./src/pets.js";
 
 const app = express();
 app.use(cors());
@@ -14,6 +14,21 @@ app.get("/pets", async (req, res) => {
         res.status(500).send(error)
     }
 })
+
+app.post("/pets", async (req, res) => {
+    try {
+        const newPet = req.body
+        // const {name, age, gender} = req.body
+        // const newPet = {name, age, gender}
+
+        const result = await createPet(newPet)
+        res.status(201).send(result);
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+    }
+})
+
 
 const PORT = 3000;
 
